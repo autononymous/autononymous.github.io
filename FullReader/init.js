75c99f0174edb890bd7d24af8eefee02f2f85de1
rkissing <r.kissinger68@gmail.com>
2025-06-26 00:19:28 -0600

    Limiting Access Revision

--- FullReader/init.js
+++ FullReader/init.js
@@ -1,23 +1,42 @@
-const ROOT = document.querySelector(':root');
 
-function RootSet(param,value) {
-  ROOT.style.setProperty(`--${param}`,value)
+const LOC   = "https://raw.githubusercontent.com/autononymous/autononymous.github.io/refs/heads/master/FullReader/config.json";
+
+
+async function initialize() 
+{
+    try {
+        const response = await fetch(LOC);
+        if (!response.ok) {
+            console.error("Error pulling init JSON.")
+        }
+        let data = await response.text();
+        let result = JSON.parse(data.replaceAll(/(\r\n|\n|\r)/gm, ''));
+        return result;
+    } catch (error) {
+        console.error("Error in fetch process for init.")
+    }
+}
+function allocate()
+{
+    const ROOT  = document.querySelector(':root');
+    let StyleNum = 0;
+    Object.values(CFG.Styles).forEach( style => {      
+        console.log(style.Header.Font)
+        ROOT.style.setProperty(`--T${StyleNum}_TitleFont`   ,style.Header.Font);
+        ROOT.style.setProperty(`--T${StyleNum}_TextFont`    ,style.Body.Font);
+        ROOT.style.setProperty(`--T${StyleNum}_SubFont`     ,style.Section.Font);    
+        ROOT.style.setProperty(`--T${StyleNum}_Background`  ,`linear-gradient(to right,${style.BackgroundGradient.Outer} 0%,${style.BackgroundGradient.Inner} 10%, ${style.BackgroundGradient.Inner} 90%, ${style.BackgroundGradient.Outer} 100%),url(${style.BackgroundGradient.Image});`);    
+        StyleNum++;
+    });
+  }
+
+async function main() 
+{
+    CFG = await initialize();
+    allocate();
 }
 
-function initialize() {  
-  
-  var CFG = "";
 
-  fetch('config.json')
-    .then(response => response.json())
-    .then(config => {
-      // Use the parsed config object as needed
-      console.log(config);
-      CFG = config;
-    })
-    .catch(error => {
-      console.error('Error loading config.json:', error);
-    });  
-}
+main();
 
-initialize();
+
